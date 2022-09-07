8c2f99ef39ebb342f225f383df1369c4ae2a0317
mldriscoll <53544431+mldriscoll@users.noreply.github.com>
2022-09-07 15:48:59 +0100

    Adding the P1V3 mapping

--- AOABM/Views/DownloadPage.xaml.cs
+++ AOABM/Views/DownloadPage.xaml.cs
@@ -136,6 +136,59 @@ namespace AOABM.Views
                 }
             },
             {
+                "ascendance-of-a-bookworm-manga-volume-3",
+                new VolumeDefinition
+                {
+                    volumeName = "Ascendance of a Bookworm (Manga) Volume 3_1440.epub",
+                    mapping = new List<VolumeDefinition.Mapping>
+                    {
+                        new VolumeDefinition.Mapping
+                        {
+                            Folder = "101-If there aren't any books, I'll just have to make some!\\100-Covers",
+                            Files = new List<Inp>
+                            {
+                                new Inp(0){NameOne = "cover.jpg"}
+                            },
+                        },
+                        new VolumeDefinition.Mapping
+                        {
+                            Folder = "101-If there aren't any books, I'll just have to make some!\\111a-I Love You, Yellow River Culture",
+                            Files = new List<Inp>{7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,46 }
+                        },
+                        new VolumeDefinition.Mapping
+                        {
+                            Folder = "101-If there aren't any books, I'll just have to make some!\\112a-The Mysterious Heat and the Meeting",
+                            Files = new List<Inp>{47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85}
+                        },
+                        new VolumeDefinition.Mapping
+                        {
+                            Folder = "101-If there aren't any books, I'll just have to make some!\\112b-Results of the Meeting",
+                            Files = new List<Inp>{86,87,88,89,90,91,92,93,94}
+                        },
+                        new VolumeDefinition.Mapping
+                        {
+                            Folder = "101-If there aren't any books, I'll just have to make some!\\113-Road to Washi",
+                            Files = new List<Inp>{97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125,126,127,128,129,130,131,132,134}
+                        },
+                        new VolumeDefinition.Mapping
+                        {
+                            Folder = "101-If there aren't any books, I'll just have to make some!\\114a-Summons from Benno",
+                            Files = new List<Inp>{135,136,137,138,139,140,141,142,143,144,145,146,147,148,149,150,151,152,153,154,155,156,157,158,159,160,161,162,163,164,165,166,167,new Inp(168,169)}
+                        },
+                        new VolumeDefinition.Mapping
+                        {
+                            Folder = "101-If there aren't any books, I'll just have to make some!\\114b-Improving the Food Situation",
+                            Files = new List<Inp>{173,174,175,176,177,178,179,180,181,182}
+                        },
+                        new VolumeDefinition.Mapping
+                        {
+                            Folder = "101-If there aren't any books, I'll just have to make some!\\111b-The Baptism Ceremony and the Hairpin",
+                            Files = new List<Inp>{184,185,186,187,188,189,190}
+                        }
+                    }
+                }
+            },
+            {
                 "ascendance-of-a-bookworm-manga-part-2-vol-1", new VolumeDefinition
                 {
                     volumeName = "Ascendance of a Bookworm (Manga) Part 2 Volume 1_1440.epub",
