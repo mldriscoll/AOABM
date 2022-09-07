a9138190d22aafe44e1c7e8a8a22072ff841cb8f
mldriscoll <53544431+mldriscoll@users.noreply.github.com>
2022-09-07 15:11:44 +0100

    Fixing bug with Covers chapters
    + Adding the P1V2 mapping in full

--- AOABM/Views/DownloadPage.xaml.cs
+++ AOABM/Views/DownloadPage.xaml.cs
@@ -106,6 +106,31 @@ namespace AOABM.Views
                         {
                             Folder = "101-If there aren't any books, I'll just have to make some!\\106-Pig Killing Day",
                             Files = new List<Inp>{5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27}
+                        },
+                        new VolumeDefinition.Mapping
+                        {
+                            Folder = "101-If there aren't any books, I'll just have to make some!\\107a-The Sweet Taste of Winter",
+                            Files = new List<Inp>{29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60}
+                        },
+                        new VolumeDefinition.Mapping
+                        {
+                            Folder = "101-If there aren't any books, I'll just have to make some!\\108-Helping Out Otto",
+                            Files = new List<Inp>{61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92}
+                        },
+                        new VolumeDefinition.Mapping
+                        {
+                            Folder = "101-If there aren't any books, I'll just have to make some!\\109-Bring Me to the Forest",
+                            Files = new List<Inp>{93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124}
+                        },
+                        new VolumeDefinition.Mapping
+                        {
+                            Folder = "101-If there aren't any books, I'll just have to make some!\\110a-Finally the Forest",
+                            Files = new List<Inp>{125,126,127,128,129,130,131,132,133,134,135,136,137,138,139,140,141,142,143,144,145,146,147,148,149,150,151,152,153,154,155,156,157,158,159,160,161,162,163,164,165}
+                        },
+                        new VolumeDefinition.Mapping
+                        {
+                            Folder = "101-If there aren't any books, I'll just have to make some!\\107b-Lutz and the Parue Tree",
+                            Files = new List<Inp>{167,168,169,170,171,172}
                         }
                     }
                 }
@@ -118,7 +143,7 @@ namespace AOABM.Views
                     {
                         new VolumeDefinition.Mapping
                         {
-                            Folder = "102-I'll even join the temple to read books!\\100-Covers",
+                            Folder = "102-I'll even join the temple to read books!\\200-Covers",
                             Files = new List<Inp>
                             {
                                 new Inp(0){NameOne = "cover.jpg"}
@@ -126,7 +151,7 @@ namespace AOABM.Views
                         },
                         new VolumeDefinition.Mapping
                         {
-                            Folder = "102-I'll even join the temple to read books!\\101-Apprentice Shrine Maiden in the Temple",
+                            Folder = "102-I'll even join the temple to read books!\\201-Apprentice Shrine Maiden in the Temple",
                             Files = new List<Inp>{5,new Inp(6,7),8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50}
                         }
                     }
